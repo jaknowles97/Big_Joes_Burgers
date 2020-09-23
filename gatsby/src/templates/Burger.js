735d944184cbbc38be3960b9872a7dcf1921773c
jaknowles97 <jaknowles1997@gmail.com>
2020-09-23 13:01:43 -0700

    further styled Burger template and included infiormation about the burger

--- gatsby/src/templates/Burger.js
+++ gatsby/src/templates/Burger.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
 
-export default function SingleBurgerPage() {
-  return <p>Single Burger!</p>;
-}
+const BurgerGrid = styled.div`
+  display: grid;
+  grid-gap: 2rem;
+  grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+`;
+
+export default function SingleBurgerPage({ data: { burger } }) {
+    return (
+      <BurgerGrid>
+        <Img fluid={burger.image.asset.fluid} />
+        <div>
+          <h2 className="mark">{burger.name}</h2>
+          <ul>
+            {burger.toppings.map((topping) => (
+              <li key={topping.id}>{topping.name}</li>
+            ))}
+          </ul>
+        </div>
+      </BurgerGrid>
+    );
+  }
 
 // This needs to be dynamic based on the slug passed in via context in gatsby-node.js
 export const query = graphql`
