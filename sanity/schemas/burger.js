065e6664460788a199ceacb7f9f405b5dc32bbd4
jaknowles97 <jaknowles1997@gmail.com>
2020-09-23 09:43:58 -0700

    added topping and person scheamas to sanity and linked sanity project to gatsaby frontend

---
+++ sanity/schemas/burger.js
@@ -0,0 +1,69 @@
+import { MdLocalPizza as icon } from 'react-icons/md';
+
+export default {
+  // Computer Name
+  name: 'burger',
+  // visible title
+  title: 'Burgers',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Burger Name',
+      type: 'string',
+      description: 'Name of burger',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+      description: 'Price of the burger in cents',
+      validation: Rule => Rule.min(1000),
+    },
+    {
+      name: 'toppings',
+      title: 'Toppings',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'topping' }] }],
+      // TODO: Add custom input component
+    },
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name',
+    },
+    prepare: ({ title, media, ...toppings }) => {
+      // 1. Filter undefined toppings out
+      const tops = Object.values(toppings).filter(Boolean);
+      // 2. return the preview object for the burger
+      return {
+        title,
+        media,
+        subtitle: tops.join(', '),
+      };
+    },
+  },
+};
--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -4,6 +4,8 @@ import createSchema from 'part:@sanity/base/schema-creator';
 import schemaTypes from 'all:part:@sanity/base/schema-type';
 
 import burger from './burger';
+import topping from './topping';
+import person from './person';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -11,5 +13,5 @@ export default createSchema({
   name: 'default',
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([burger]),
+  types: schemaTypes.concat([burger,topping, person]),
 });
