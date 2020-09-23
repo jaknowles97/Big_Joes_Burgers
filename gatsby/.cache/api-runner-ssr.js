2c6696ba3741b38b82f911fab6b09c11a081943c
jaknowles97 <jaknowles1997@gmail.com>
2020-09-23 09:00:20 -0700

    added template and global styles

--- gatsby/.cache/api-runner-ssr.js
+++ gatsby/.cache/api-runner-ssr.js
@@ -1,4 +1,7 @@
-var plugins = []
+var plugins = [{
+      plugin: require('C:/Users/andrew/personalProjects/Big_Joes_Burgers/gatsby/gatsby-ssr'),
+      options: {"plugins":[]},
+    }]
 // During bootstrap, we write requires at top of this file which looks like:
 // var plugins = [
 //   {
